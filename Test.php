0c6608e14a5f1000ed1ca823eb6a26cb6d025356
dongasai <1514582970@qq.com>
2019-04-15 16:55:30 +0800

    dmeo

--- Test.php
+++ Test.php
@@ -5,5 +5,6 @@ require __DIR__ . '/UPnP.php';
 $upnp = new UPnP();
 var_dump($upnp->routerIPAddress ());
 var_dump($upnp->getExternalIPAddress());
+
 var_dump($upnp->addPortMapping (30045, 30046, $upnp->getExternalIPAddress()));
 var_dump($upnp->deletePortMapping (30045));
